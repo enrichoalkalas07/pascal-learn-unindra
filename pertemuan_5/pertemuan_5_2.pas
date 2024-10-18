1846dd81aed75bcaef527ca05ffb77c6fde00601
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2024-10-18 19:36:03 +0700

    add: bilangan positif & negatif

---
+++ pertemuan_5/pertemuan_5_2.pas
@@ -0,0 +1,31 @@
+program positif_negatif;
+uses crt;
+
+var bilangan : int64;
+var keterangan : string;
+
+begin
+    clrscr;
+    writeln('Masukan Nilai Bilangan : ');
+    readln(bilangan);
+
+    clrscr;
+
+    if ( bilangan = 0 ) then
+        begin
+            keterangan := 'Bilangan Genap';
+        end
+    else
+        begin
+            if ( bilangan > 0 ) then
+                begin
+                    keterangan := 'Bilangan adalah positify';
+                end
+            else
+                begin
+                    keterangan := 'Bilangan adalah positify';
+                end;
+        end;
+
+    writeln('Hasil Kalkulasi : ', keterangan);
+end.
