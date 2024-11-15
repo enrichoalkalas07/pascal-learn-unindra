ffe2605b964e85dad6b11f8f9a33ccc1e498c155
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2024-11-15 19:47:15 +0700

    test from source tree

--- pertemuan_12/pertemuan_12_1.pas
+++ pertemuan_12/pertemuan_12_1.pas
@@ -7,12 +7,13 @@ begin
     clrscr;
     i := 10;
     // Perulangan Dari Terbesar Ke Terkecil
-    // while ( i >= 1 ) do
-        // begin
-            // writeln(i);
-            // i := i - 1;
-        // end;
-    
+    while ( i >= 1 ) do
+        begin
+            writeln(i);
+            i := i - 1;
+        end;
+
+    // Perulangan Dari Terbesar Ke Terkecil Kelipatan 5
     i := 25;
     while ( i >= 0 ) do 
         begin
