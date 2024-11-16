547478ae52f2991c43499414349f0231d6ed9efc
enrichoalkalas01 <enrichoalkalas01@gmail.com>
2024-11-16 08:01:13 +0700

    no message

--- pertemuan_12/pertemuan_12_1.pas
+++ pertemuan_12/pertemuan_12_1.pas
@@ -2,17 +2,20 @@ program ulang;
 uses crt;
 
 var i : int64;
+var n : int64;
 
 begin
     clrscr;
-    i := 10;
+    writeln('===================================');
     // Perulangan Dari Terbesar Ke Terkecil
+    i := 10;
     while ( i >= 1 ) do
         begin
             writeln(i);
             i := i - 1;
         end;
 
+    writeln('===================================');
     // Perulangan Dari Terbesar Ke Terkecil Kelipatan 5
     i := 25;
     while ( i >= 0 ) do 
@@ -20,4 +23,7 @@ begin
             writeln(i);
             i := i - 5;
         end;
+
+    writeln('===================================');
+    
 end.
---
+++ pertemuan_12/pertemuan_12_2.pas
@@ -0,0 +1,36 @@
+program LoopingWhileDo;
+uses crt;
+
+var
+  i, j: Integer;
+
+begin
+    clrscr;
+    i := 1;
+
+    while i <= 5 do
+        begin
+            j := 1;
+            while j <= i do
+        begin
+            write(j, ' ');
+            j := j + 1;
+        end;
+
+        writeLn;
+        i := i + 1;
+    end;
+
+    i := 5;
+    while i >= 1 do
+        begin
+            j := 5;
+            while j >= i do
+                begin
+                    write(j, ' ');
+                    j := j - 1;
+                end;
+                writeLn;
+                i := i - 1;
+        end; 
+end.
